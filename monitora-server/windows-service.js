886775035c1593645ea04b62a669e78760e10e9b
Felipe G.Reis <freis2@populis.local>
2017-02-02 14:53:13 -0200

    arquivos de daemon de servicos windows adicionados pelo glr-gestor

---
+++ monitora-server/windows-service.js
@@ -0,0 +1,16 @@
+﻿var Service = require('node-windows').Service;
+
+// Create a new service object
+var svc = new Service({
+  name:'Monitora Server',
+  description: 'Parte servidor do Monitora, responsavel por fazer a verificacao nas bases dos clientes',
+  script: 'E:\\Populis_SI_Data\\Monitora\\monitora-repo\\monitora-server\\index.js'
+});
+
+// Listen for the "install" event, which indicates the
+// process is available as a service.
+svc.on('install',function(){
+  svc.start();
+});
+
+svc.install();
