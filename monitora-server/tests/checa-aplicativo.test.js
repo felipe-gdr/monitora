5f6bd4164db624ad9b358f2d1b05ab31ba0c52b6
Felipe Reis <felipe@glr.com.br>
2017-04-13 10:13:16 -0300

    test(server): Add 'describes' to test case for better organization

--- monitora-server/tests/checa-aplicativo.test.js
+++ monitora-server/tests/checa-aplicativo.test.js
@@ -10,40 +10,54 @@ describe('ChecaAplicativo', function() {
     app = appComplete()
   });
 
-  it('monta url final para Populis I', function() {
-    var urlFinal = new ChecaAplicativo(app).urlFinal('http://localhost:8080/populis');
-    expect(urlFinal).to.equal('http://localhost:8080/populis/informacoes.jsp');
+  describe('urlFinal()', function() {
+    it('monta url final para Populis I', function() {
+      var urlFinal = new ChecaAplicativo(app).urlFinal('http://localhost:8080/populis');
+      expect(urlFinal).to.equal('http://localhost:8080/populis/informacoes.jsp');
+    });
+
+    it('monta url final para Populis II', function() {
+      app.nome = 'Populis II'
+      var urlFinal = new ChecaAplicativo(app).urlFinal('http://localhost:8080/populisII-web');
+      expect(urlFinal).to.equal('http://localhost:8080/populisII-web/rest/administracao/info');
+    });
   });
 
-  it('monta url final para Populis II', function() {
-    app.nome = 'Populis II'
-    var urlFinal = new ChecaAplicativo(app).urlFinal('http://localhost:8080/populisII-web');
-    expect(urlFinal).to.equal('http://localhost:8080/populisII-web/rest/administracao/info');
+  describe('label()', function() {
+    it('monta label para aplicativo', function() {
+      var label = new ChecaAplicativo(app).label(app);
+      expect(label).to.equal('Empresa 2 : Populis I');
+    });
+
+    it('monta label para node', function() {
+      var label = new ChecaAplicativo(app).label(app.cluster.master);
+      expect(label).to.equal('master');
+    });
   });
 
-  it('monta label para aplicativo', function() {
-    var label = new ChecaAplicativo(app).label(app);
-    expect(label).to.equal('Empresa 2 : Populis I');
-  });
+  describe('parseDetalhesServidor()', function() {
+    it('parse detalhes servidor Populis II', function() {
+      app.nome = 'Populis II'
 
-  it('monta label para node', function() {
-    var label = new ChecaAplicativo(app).label(app.cluster.master);
-    expect(label).to.equal('master');
-  });
+      var detalhesServidor = new ChecaAplicativo(app).parseDetalhesServidor(JSON.stringify(app.detalhesServidor))
 
-  it('parse detalhes servidor Populis II', function() {
-    app.nome = 'Populis II'
+      expect(detalhesServidor).to.deep.equal(app.detalhesServidor);
+    });  
 
-    var detalhesServidor = new ChecaAplicativo(app).parseDetalhesServidor(JSON.stringify(app.detalhesServidor))
+    it('parse detalhes servidor Populis II', function() {
+      var body = '<html><body class="Xbody"><table cellspacing="2" width="100%"><tbody><tr><td>&nbsp;{"detalhes": "esses são detalhes"}</td></tr></tbody></table></body></html>'
 
-    expect(detalhesServidor).to.deep.equal(app.detalhesServidor);
-  });  
+      var detalhesServidor = new ChecaAplicativo(app).parseDetalhesServidor(body)
 
-  it('parse detalhes servidor Populis II', function() {
-    var body = '<html><body class="Xbody"><table cellspacing="2" width="100%"><tbody><tr><td>&nbsp;{"detalhes": "esses são detalhes"}</td></tr></tbody></table></body></html>'
+      expect(detalhesServidor).to.deep.equal({"detalhes": "esses são detalhes"});
+    });
 
-    var detalhesServidor = new ChecaAplicativo(app).parseDetalhesServidor(body)
+    it('parse detalhes servidor Populis II', function() {
+      var body = '<html><body class="Xbody"><table cellspacing="2" width="100%"><tbody><tr><td>&nbsp;{"detalhes": "esses são detalhes"}</td></tr></tbody></table></body></html>'
 
-    expect(detalhesServidor).to.deep.equal({"detalhes": "esses são detalhes"});
+      var detalhesServidor = new ChecaAplicativo(app).parseDetalhesServidor(body)
+
+      expect(detalhesServidor).to.deep.equal({"detalhes": "esses são detalhes"});
+    });
   });
 });
